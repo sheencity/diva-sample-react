23f2c74c70620b67bb5760b1e3fe090092917343
HeQin <heqin@sheencity.com>
2021-09-13 17:40:00 +0800

    chore: 更改副标题

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -17,7 +17,7 @@ export default class Header extends Component {
             <div className="header">
                 <div className="title">
                     <div className="cn">光辉城市数据智能可视化分析接口演示</div>
-                    <div className="en">SHEENCITY DIVA (DATA INTELLIGENCE VISUAL ANALYSIS) API DEMO</div>
+                    <div className="en">SHEENCITY DIVA (Data Intelligent Visual API) DEMO</div>
                 </div>
                 <div className="info">
                     <div className="exampleCode">
